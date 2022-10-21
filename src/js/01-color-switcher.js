b50f3326063a6f01f9cc80e0ff92f7af96c87c84
Nastyona22 <nastyona.savina@gmail.com>
2022-10-21 16:31:50 +0300

    First task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const btnStartRef = document.querySelector('.js-start-btn');
+const btnStopRef = document.querySelector('.js-stop-btn');
+const body = document.querySelector("body");
+let timerId = null;
 
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColorStart() {
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+        btnStartRef.disabled = true;
+         btnStopRef.disabled = false;
+
+  }, 1000);
+    
+};
+
+btnStartRef.addEventListener('click', changeColorStart);
+
+
+
+
+btnStopRef.addEventListener("click", () => {
+  clearInterval(timerId);
+    btnStartRef.disabled = false;
+    btnStopRef.disabled = true;
+
+});
